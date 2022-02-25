bc21652177d60537d7c0445a5c4543489418ee40
yuri1709 <yuriconder184@gmail.com>
2022-02-24 22:13:34 -0300

    excluido confirmar e validation

--- validation.js
+++
@@ -1,9 +0,0 @@
-function validar(){
-    if((document.login.usuario.value == "Admin")&&(document.login.senha.value == "123")){ 
-       return true ;
-    }else {
-       document.login.senha.focus();
-       alert("Senha ou usúario inválido!");
-        return false;
-    }
- }
