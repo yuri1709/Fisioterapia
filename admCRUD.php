6cc8da73a1dd1ce67e347e4ae4e1503cf73ed9db
yuri1709 <yuriconder184@gmail.com>
2022-03-01 02:06:23 -0300

    interface do painel admin pronta

--- admCRUD.php
+++ admCRUD.php
@@ -1,3 +1,7 @@
+<?php
+    include_once "head.php";
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,33 +10,67 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fisioterapeuta</title>
     <style>
-    .c1{
-        padding-bottom:80px;
-    }
-    .paciente{
-        float:left;
-        width:40%;
-        background:white;
-    }
-    .medico{
-        float:right;
-        width:40%;
-        background:white;
-    }
-</style>
+        .c1{
+            padding-bottom:80px;
+        }
+       
+    </style>
+    <link rel="stylesheet" href="css/admCRUD.css">
 </head>
 <body>
-<?php
-    include_once "head.php";
-    session_start();
-?>
+<section>
   <div class="container c1">
-        <h2>Painel Admin</h2>
+    <h2>Painel Admin</h2>
+    <div class="container painel my-4">
+        <form class="form my-2">
+            <div class="form-group search">
+                <input type="text" name="" id="" class="form-control" placeholder="nome"> 
+            </div>
+            <button type="submit" class="btn btn-primary mx-2 px-2">Buscar</button>
+            
+            <div class="form-group perfil">
+            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <option value="1">Fisioterapeuta</option>
+            <option value="2">Paciente</option>
+            </select>
+            </div>
+       
+        
+        <!--Mostrar resultado da pesquisa-->
+        <div class="container resultado">
+
+        </div>
+
+        <div class="container mostrarTodos">
+            <h6 class="text-center">mostrar todos os resultados</h6>
+        </div>
+        
+        <div class="container botoes py-4">
+            <div class="row text-center">
+                <div class="col">
+                <button type="submit" class="btn btn-success mx-2 px-2">CRIAR</button>
+                </div>
+                <div class="col">
+                <button type="submit" class="btn btn-warning mx-2 px-2">EDITAR</button>
+                </div>
+                <div class="col">
+                <button type="submit" class="btn btn-danger mx-2 px-2">EXLUIR</button>
+                </div>
+            </div>
+    
+        </div>
+        </form>
+    </div>
   </div>
- <div class="container c2">
+  </section> 
+ 
+
+
+
+ <!-- <div class="container c2">
  <a href="cadastroPaciente.php">Cadastrar Paciente</a> </br>
  <a href="editarPaciente.php">Editar cadastro</a> </br>
  <a href="sair.php">Sair</a>
- </div>
+ </div> -->
 </body>
 </html>
