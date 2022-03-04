fe2062144190bf5d09ba932b4b881b87963f1c2e
yuri1709 <yuriconder184@gmail.com>
2022-03-03 22:16:18 -0300

    add usuarioGeral.php, new logo in admPainel and fixed head.php

--- admCRUD.php
+++ admCRUD.php
@@ -15,20 +15,22 @@
     <title>Fisioterapeuta</title>
     <style>
         .c1{
-            padding-bottom:80px;
+        padding-bottom:80px;
         }
        .c2{
         padding-top:500px;
        }
-       
+       .logout{
+          float:right;
+       }
     </style>
 <link rel="stylesheet" href="css/admCRUD.css">
 </head>
 <body>
-<section>
+<section>   
   <div class="container c1">
-    <h2>Painel Admin</h2>
-    <div class="container painel my-4">
+   <div class="container header"><h2>Painel Admin <a href="sair.php" class="logout"><img src="img/admPainel/logout.png" alt="" width="36px" height="36px"></a> </h2></div> 
+    <div class="container painel my-2">
         <form class="form my-2" action="" method="POST"  autocomplete="off">
             <div class="form-group search">
                 <input type="text" name="nome" id="nome" class="form-control" placeholder="buscar o nome do usuário"> 
@@ -72,7 +74,9 @@
         <div class="container botoes py-4">
             <div class="row text-center">
                 <div class="col">
-                    <button type="submit" class="btn btn-success mx-2 px-2">CRIAR</button>
+                <a href="criarUsuarioGeral.php">  
+                    <button type="button" class="btn btn-success mx-2 px-2">CRIAR</button>
+                </a>
                 </div>
                 <div class="col">
                 <a href="editarUsuarioGeral.php?nome=<?php echo base64_encode($nome); ?>&idUser=<?php echo base64_encode($idUser); ?>">
@@ -93,9 +97,7 @@
   </div>
 </section> 
 <section>
-    <div class="container c2">
-       <h4> <a href="sair.php">Sair</a>
-    </div> 
+   
 </section>
 
   
--- criarUsuarioGeral.php
+++ criarUsuarioGeral.php
@@ -1,19 +1,11 @@
 <?php
-  session_start();
-  include 'auth.php';
-  include_once "conexao.php";
+    session_start(); 
+    include_once "auth.php";
+   
+    include_once "conexao.php";
+    
 
-  $login = base64_decode($_GET["nome"]);
-  $idUser = base64_decode($_GET["idUser"]);
-  
-  $sql = "select * from usuario where idUser=".$idUser;
-  //excutar
-  $rs = mysqli_query($con,$sql);
-  if(mysqli_num_rows($rs) == 1){
-    $reg = mysqli_fetch_array($rs);
-}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,14 +15,15 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <title>Document</title>
 </head>
-<body>
+<style>
+</style>    
 <?php  include_once "head.php"; ?>
+<body>
   <div class="container c1">
-    <h2><?php echo "Editar usuário: ".$login; ?><a href="admCRUD.php"><button type="button" class="btn btn-success float-right">Sair</button></a></h2>
     
-  <div class="container-flex">
-  <div class="container bg-light">
-   <form class="form-group" action="atualizarUsuarioGeral.php" method="POST">
+  <div class="container">
+  <div class="container teste bg-light ">
+   <form class="form-group teste" action="atualizarUsuarioGeral.php" method="POST">
   <!-- Login input -->
   <div class="row mb-4">
     <div class="col">
@@ -73,24 +66,12 @@
       </select>
   </div>
 
-
- <!-- <div class="form-outline mb-4">
-    <label class="form-label" for="form6Example8">Cursos:</label></br>
-  <input type ="checkbox" name="vetor[]" value="PHP"> PHP
-  <input type ="checkbox" name="vetor[]" value="Designer"> Designer
-  <input type ="checkbox" name="vetor[]" value="LARAVEL"> LARAVEL
-  <input type ="checkbox" name="vetor[]" value="MYSQL"> MYSQL
-  <input type ="checkbox" name="vetor[]" value="ORACLE"> ORACLE
-        <br/>
-  </div> -->
-
   <!-- Submit button -->
  <div class="py-4">
   <input type="reset" value="Limpar"/ class="btn btn-danger">
   <input type="submit" value="Enviar"/ class="btn btn-primary">
 </div> 
 <!--responsavel por enviar o idUser para a pagina atualizarUsuarioGeral-->
-<input type="hidden" name="idUser" value="<?php echo $reg["idUser"];?>;"/> 
 </form>
 </div>
   </div>
