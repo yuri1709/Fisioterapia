4622ff15dae3bf05351ccc2df5c7d6fabd4f411c
yuri1709 <yuriconder184@gmail.com>
2022-04-12 21:34:39 -0300

    Esqueleto da página de Endereço

---
+++ criarUsuarioEndereco.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <title>Criar Usuário</title>
+</head>
+<style>
+.c2{
+  padding-top:20px;
+}
+.c3{
+  padding-top:20px;
+}
+</style>    
+<?php  include_once "head.php"; ?>
+<body>
+<div class="container c1">
+    <h2>Hello World</h2>
+</div>
+     
+<div class="container c2">
+    <h2>Hello World</h2>
+</div>
+
+<div class="container c3">
+    <h2>Hello World</h2>
+</div>
+
+
+</body>
+</html>
