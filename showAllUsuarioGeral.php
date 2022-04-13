ea2080282ce1698c7549639d40bd89629acea073
yuri1709 <yuriconder184@gmail.com>
2022-04-13 02:36:43 -0300

    Melhorias: Responsivo, e filtro
    Adicionado jQuery Filters
    Melhoria na responsividade da página

--- showAllUsuarioGeral.php
+++ showAllUsuarioGeral.php
@@ -15,7 +15,7 @@ $perfil = $_POST['perfil'];
 <body>
     <div class="container">
         <?php switch($perfil){case '1':$perfilNome = "Fisioterapeuta";break;case '2':$perfilNome = "Paciente";break;}?>
-        <h3 class="text-center bg primary">Lista de <?php echo $perfilNome;?> </h3><a class="text float-right" href="admCRUD.PHP">voltar</a>
+        <h3 class="text-center">Lista de <?php echo $perfilNome;?> </h3><a class="text float-right" href="admCRUD.PHP">voltar</a>
 
         <?php
         $sql = "select * from usuario WHERE perfil= '$perfil'";
@@ -25,14 +25,30 @@ $perfil = $_POST['perfil'];
         if($totalRegistro > 0){
             echo "Total: ".$totalRegistro." registros";
            ?>
+           <!-- INSERIR FILTER-TABLE -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+            <script>
+                $(document).ready(function(){
+                $("#myInput").on("keyup", function() {
+                    var value = $(this).val().toLowerCase();
+                    $("#myTable tr").filter(function() {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                    });
+                });
+                });
+            </script>
+            <br>
+            <div class="text-center"><input id="myInput" class="col-md-8" type="text" placeholder="Buscar por registros..."></div>
+            <br><br>
+        <div class="table-responsive">
            <table table class="table table-striped my-3">
                <tr>
-                   <th>id</th>
-                   <th>nome</th>
-                   <th>sobrenome</th>
-                   <th>login</th>
-                   <th>email</th>
-                   <th>perfil</th>
+                   <th scope="col">id</th>
+                   <th scope="col">nome</th>
+                   <th scope="col">sobrenome</th>
+                   <th scope="col">login</th>
+                   <th scope="col">email</th>
+                   <th scope="col">perfil</th>
                </tr>
             <?php
             while($linha = mysqli_fetch_array($result)){
@@ -40,6 +56,7 @@ $perfil = $_POST['perfil'];
                     $linha["perfil"]="Fisioterapeuta";
                 }
                 ?>
+                <tbody id="myTable">
                 <tr>
                     <td><?php echo $linha["idUser"]?></td>
                     <td><?php echo $linha["nome"]?></td>
@@ -48,11 +65,12 @@ $perfil = $_POST['perfil'];
                     <td><?php echo $linha["email"]?></td>
                     <td><?php echo $perfilNome?></td>
                 </tr>
-          
-           
+                </tbody>
             <?php
             }
-            ?></table><?php  
+            ?></table>
+        </div>
+            <?php  
         }else{
             echo"Não existe usuário cadastrado"; 
         }
